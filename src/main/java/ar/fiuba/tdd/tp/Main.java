641e64b561055f98e4c30c7e67a11f983ca9c797
keynaka <keynaka@gmail.com>
2016-04-16 12:07:47 -0300

    Verificando git push con un simple System.out

--- src/main/java/ar/fiuba/tdd/tp/Main.java
+++ src/main/java/ar/fiuba/tdd/tp/Main.java
@@ -3,5 +3,8 @@ package ar.fiuba.tdd.tp;
 public class Main {
     public static void main(String[] args) {
         System.out.println("This is just a tp project");
+        System.out.println("testing git push");
+
     }
+
 }
