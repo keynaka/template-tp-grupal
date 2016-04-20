420be3f8a960003ddb308f4e1092cba462734400
keynaka <keynaka@gmail.com>
2016-04-19 22:01:22 -0300

    Implemento primer juego, Fetch Quest, utilizando los metodos abstracto a definir del GameTemplate.

---
+++ src/main/java/ar/fiuba/tdd/tp/ReceiveNegativeCountOfInformation.java
@@ -0,0 +1,7 @@
+package ar.fiuba.tdd.tp;
+
+public class ReceiveNegativeCountOfInformation extends Exception {
+    public ReceiveNegativeCountOfInformation(String msg) {
+        super(msg);
+    }
+}
