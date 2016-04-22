8a2fd44788233f91d77d4a5851340918a454b1f7
keynaka <keynaka@gmail.com>
2016-04-22 18:37:40 -0300

    Implemento Main cliente y servidor, simple, solo por camino feliz

---
+++ src/main/java/ar/fiuba/tdd/tp/MainClient.java
@@ -0,0 +1,37 @@
+package ar.fiuba.tdd.tp;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class MainClient {
+    public static void main(String[] args) {
+
+        Client oneClient = new Client();
+        oneClient.createSocket();
+        oneClient.createInputOutputThreads();
+
+        Scanner scanner = new Scanner(System.in,"UTF-8");
+
+        String msg;
+        while (true) {
+            System.out.println("Write message to send server: ");
+            msg = scanner.nextLine();
+
+            oneClient.getOutThread().println(msg);
+
+            if ("FIN".equals(msg)) {
+                break;
+            }
+            try {
+                msg = oneClient.getInThread().readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("Server Message Received: " + msg);
+        }
+
+        oneClient.close();
+        scanner.close();
+    }
+}
---
+++ src/main/java/ar/fiuba/tdd/tp/MainServer.java
@@ -0,0 +1,33 @@
+package ar.fiuba.tdd.tp;
+
+import java.io.IOException;
+
+public class MainServer {
+    public static void main(String[] args) {
+        Server oneServer = new Server();
+        oneServer.createSocket();
+        oneServer.waitClient();
+        oneServer.createInputOutputThreads();
+
+        String answer = "Message Received Correctly --> ";
+        String msg = "";
+        while (true) {
+            try {
+                msg = oneServer.getInThread().readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            if ("FIN".equals(msg)) {
+                break;
+            } else {
+                System.out.println("Mensaje del cliente [" + oneServer.getSocket().getInetAddress().getHostName() + "] : "
+                        + msg);
+
+                oneServer.getOutThread().println(answer + msg);
+            }
+        }
+        oneServer.close();
+    }
+}
+
