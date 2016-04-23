67ed245f117babf138da9e17b70d2bc996610a6f
swandelow <swandelow@despegar.com>
2016-04-23 14:33:57 -0300

    borro vieja version de fetchquest

--- src/main/java/ar/fiuba/tdd/tp/Main.java
+++ src/main/java/ar/fiuba/tdd/tp/Main.java
@@ -3,8 +3,6 @@ package ar.fiuba.tdd.tp;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Fetch Quest \n");
-        GameTemplate game1 = new FetchQuest();
-        game1.playOneGame();
     }
 
 }
