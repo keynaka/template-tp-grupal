e71704df0a0ef9ecd97d74eeaeaf2e5f966322e6
Patricia Machado <pmachado194@gmail.com>
2016-04-25 23:33:28 -0300

    Refactorizo HanoiTowers

--- src/main/java/ar/fiuba/tdd/tp/games/items/Disk.java
+++ src/main/java/ar/fiuba/tdd/tp/games/items/Disk.java
@@ -1,18 +1,45 @@
 package ar.fiuba.tdd.tp.games.items;
 
 /**
- * Created by Patri on 23/04/2016.
+ * Created by Patri on 24/04/2016.
  */
-public class Disk {
+public class Disk extends Item {
 
-    private int size;
+    Disk disk;
 
-    public Disk(int size) {
-        this.size = size;
+    /*
+    *  Creates disk with the size received in 'description'
+    *  If description cannot be converted to int, disk is null.
+    */
+    public Disk(String name, String description) {
+
+        super(name, description);
+        try {
+            int size = Integer.parseInt(description);
+            disk = new Disk(size);
+        } catch (NumberFormatException e) {
+            disk = null;
+        }
     }
 
-    public int getSize() {
-        return size;
+    /* Returns disk size, if disk is null it throws a NumberFormatException */
+    public int getSize() throws NumberFormatException {
+        if (disk != null) {
+            return this.disk.getSize();
+        } else {
+            throw new NumberFormatException();
+        }
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
 
 }
