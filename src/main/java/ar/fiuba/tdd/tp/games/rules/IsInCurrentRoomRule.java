06c79104e6e265ab5b467386b8dfd0c4785c4e1b
swandelow <swandelow@despegar.com>
2016-05-31 11:28:29 -0300

    agrego IsInCurrentRoomRule

---
+++ src/main/java/ar/fiuba/tdd/tp/games/rules/IsInCurrentRoomRule.java
@@ -0,0 +1,25 @@
+package ar.fiuba.tdd.tp.games.rules;
+
+import ar.fiuba.tdd.tp.games.ConcreteGame;
+import ar.fiuba.tdd.tp.games.items.Item;
+import ar.fiuba.tdd.tp.games.objects.GameObject;
+
+/**
+ * Created by swandelow on 5/31/16.
+ */
+public class IsInCurrentRoomRule extends Rule {
+
+    private ConcreteGame game;
+    private String itemName;
+
+    public IsInCurrentRoomRule(ConcreteGame game, String itemName) {
+        this.game = game;
+        this.itemName = itemName;
+    }
+
+    @Override
+    public boolean doVerify() {
+        Item item = (Item) this.game.getGameObjectRepository().getGameObject(itemName);
+        return new HasItemRule(this.game.getCurrentStage(), item).doVerify();
+    }
+}
