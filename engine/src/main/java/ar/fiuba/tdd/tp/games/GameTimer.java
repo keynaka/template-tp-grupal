b927795f41556edd1e53b4f1e2260bd90e39b376
keynaka <keynaka@gmail.com>
2016-06-07 21:20:11 -0300

    Implementacion de un Timer

---
+++ engine/src/main/java/ar/fiuba/tdd/tp/games/GameTimer.java
@@ -0,0 +1,35 @@
+package ar.fiuba.tdd.tp.games;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class GameTimer {
+    Timer timer;
+    int seconds;
+
+    public GameTimer(int seconds) {
+        this.seconds = seconds;
+    }
+
+    public void startTimer() {
+        timer = new Timer();
+        timer.schedule(new ChangeState(), this.seconds*1000, this.seconds*1000);
+    }
+
+    public void stopTimer() {
+        timer.cancel();
+    }
+
+    class ChangeState extends TimerTask {
+        public void run() {
+            System.out.println( "OK, It's time to do something!" );
+        }
+    }
+
+    public static void main(String args[]) {
+        System.out.println("Schedule something to do in 2 seconds.");
+        GameTimer gt = new GameTimer(2);
+        gt.startTimer();
+        System.out.println("Waiting.");
+    }
+}
