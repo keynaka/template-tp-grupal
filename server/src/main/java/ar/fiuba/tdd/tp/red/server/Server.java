c4cb19b6e8776e253ef82b029eb3f6fcd5dee940
Gonzalo Vallerino <Gonzalo Vallerino>
2016-06-08 20:57:09 -0300

    Levantar cualquier jar

--- server/src/main/java/ar/fiuba/tdd/tp/red/server/Server.java
+++ server/src/main/java/ar/fiuba/tdd/tp/red/server/Server.java
@@ -45,8 +45,8 @@ public class Server {
 
     private void loadGame(String gameName) {
         try {
-            //GameBuilder gameBuilder = BuilderLoader.load(gameName);
-            GameBuilder gameBuilder = new FetchQuestBuilder();
+            GameBuilder gameBuilder = BuilderLoader.load(gameName);
+            //GameBuilder gameBuilder = new FetchQuestBuilder();
             listenPort(defaultPort++, gameBuilder);
 
             System.out.println('"' + gameName + '"' + " loaded and listening on port " + (defaultPort - 1));
