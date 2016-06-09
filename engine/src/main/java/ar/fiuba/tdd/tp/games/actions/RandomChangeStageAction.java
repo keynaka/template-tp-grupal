9494391fe41a71281fd8a713f7a82c72b8470cf2
sebass <swandelow@despegar.com>
2016-06-09 01:22:10 -0300

    agrego que el player pierda al encontrarse con el bibliotecario

--- engine/src/main/java/ar/fiuba/tdd/tp/games/actions/RandomChangeStageAction.java
+++ engine/src/main/java/ar/fiuba/tdd/tp/games/actions/RandomChangeStageAction.java
@@ -6,6 +6,9 @@ import ar.fiuba.tdd.tp.games.items.Item;
 
 import java.util.Random;
 
+import static ar.fiuba.tdd.tp.games.escape.EscapeProperties.ALLOWED_IN_LIBRARY_STATUS;
+import static ar.fiuba.tdd.tp.games.escape.EscapeProperties.NOT_ALLOWED;
+
 /**
  * Created by swandelow on 6/8/16.
  */
@@ -24,13 +27,22 @@ public class RandomChangeStageAction implements Action {
         Random random = new Random();
         Stage currentStage = this.game.getGameObjectRepository().getCurrentStageFor(this.itemName);
 
-        int i = random.nextInt(currentStage.getConsecutiveStages().size());
-        String nextStageName = currentStage.getConsecutiveStages().get(i);
+        int randomInt = random.nextInt(currentStage.getConsecutiveStages().size());
+        String nextStageName = currentStage.getConsecutiveStages().get(randomInt);
         Stage nextStage = this.game.getStage(nextStageName);
 
         System.out.println(String.format("%s is now in %s", itemName, nextStageName));
 
         Item item = currentStage.removeItem(itemName);
         nextStage.addItem(item);
+
+        boolean IsPlayerAndLibrarianInSameRoom = this.game.getPlayer().getCurrentStage().equalsIgnoreCase(nextStageName);
+        boolean notAllowedInLibrary = this.game.getPlayer().getState(ALLOWED_IN_LIBRARY_STATUS).equalsIgnoreCase(NOT_ALLOWED);
+
+        if (IsPlayerAndLibrarianInSameRoom && notAllowedInLibrary) {
+            this.game.getPlayer().setHasLost();
+            System.out.println(String.format("%s has lost. Librarian has checked invalid id card.", itemName, nextStageName));
+        }
+
     }
 }
