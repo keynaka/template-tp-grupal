154a5986e8c7955d3eb8d7ddbd6b4d6d80d11172
sebass <swandelow@despegar.com>
2016-06-14 19:36:08 -0300

    agrego paquete de test

---
+++ escape2/src/test/java/ar/fiuba/tdd/tp/games/escape/Escape2Test.java
@@ -0,0 +1,14 @@
+package ar.fiuba.tdd.tp.games.escape;
+
+import org.junit.Test;
+
+/**
+ * Created by sebass on 14/06/16.
+ */
+public class Escape2Test {
+
+    @Test
+    public void test() {
+        
+    }
+}
